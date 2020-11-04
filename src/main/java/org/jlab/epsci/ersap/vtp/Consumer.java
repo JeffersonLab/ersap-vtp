69eb66f9d88b2ab05e1a0e6eb8a80a8abad96679
gurjyan <gurjyan@jlab.org>
2020-11-04 11:20:15 -0500

    - debugging

--- src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
@@ -82,42 +82,43 @@ public class Consumer extends Thread {
         bb.order(ByteOrder.LITTLE_ENDIAN);
         int[] slot_ind = new int[8];
         int[] slot_len = new int[8];
-        long tag = EUtil.getUnsignedInt(bb);
-        if ((tag & 0x8FFF8000L) == 0x80000000L) {
-
-            for (int jj = 0; jj < 8; jj++) {
-                slot_ind[jj] = EUtil.getUnsignedShort(bb);
-                slot_len[jj] = EUtil.getUnsignedShort(bb);
-            }
-
-            for (int i = 0; i < 8; i++) {
-                if (slot_len[i] > 0) {
-                    bb.position(slot_ind[i] * 4);
-                    int type = 0;
-                    for (int j = 0; j < slot_len[i]; j++) {
-                        int val = bb.getInt();
-                        AdcHit hit = new AdcHit();
-
-                        if ((val & 0x80000000) == 0x80000000) {
-                            type = (val >> 15) & 0xFFFF;
-                            hit.setCrate((val >> 8) & 0x007F);
-                            hit.setSlot((val) & 0x001F);
-                        } else if (type == 0x0001) /* FADC hit type */ {
-                            hit.setQ((val) & 0x1FFF);
-                            hit.setChannel((val >> 13) & 0x000F);
-                            long v = ((val >> 17) & 0x3FFF) * 4;
-                            BigInteger ht = BigInteger.valueOf(v);
-                            hit.setTime(frame_time_ns.add(ht));
-                            hit.setTime(ht);
-                            res.add(hit);
-                        }
-                    }
-                }
-            }
-        } else {
-            System.out.println("parser error: wrong tag");
-            System.exit(0);
-        }
+        System.out.println(payload.length);
+//        long tag = EUtil.getUnsignedInt(bb);
+//        if ((tag & 0x8FFF8000L) == 0x80000000L) {
+//
+//            for (int jj = 0; jj < 8; jj++) {
+//                slot_ind[jj] = EUtil.getUnsignedShort(bb);
+//                slot_len[jj] = EUtil.getUnsignedShort(bb);
+//            }
+//
+//            for (int i = 0; i < 8; i++) {
+//                if (slot_len[i] > 0) {
+//                    bb.position(slot_ind[i] * 4);
+//                    int type = 0;
+//                    for (int j = 0; j < slot_len[i]; j++) {
+//                        int val = bb.getInt();
+//                        AdcHit hit = new AdcHit();
+//
+//                        if ((val & 0x80000000) == 0x80000000) {
+//                            type = (val >> 15) & 0xFFFF;
+//                            hit.setCrate((val >> 8) & 0x007F);
+//                            hit.setSlot((val) & 0x001F);
+//                        } else if (type == 0x0001) /* FADC hit type */ {
+//                            hit.setQ((val) & 0x1FFF);
+//                            hit.setChannel((val >> 13) & 0x000F);
+//                            long v = ((val >> 17) & 0x3FFF) * 4;
+//                            BigInteger ht = BigInteger.valueOf(v);
+//                            hit.setTime(frame_time_ns.add(ht));
+//                            hit.setTime(ht);
+//                            res.add(hit);
+//                        }
+//                    }
+//                }
+//            }
+//        } else {
+//            System.out.println("parser error: wrong tag");
+//            System.exit(0);
+//        }
         return res;
     }
 
