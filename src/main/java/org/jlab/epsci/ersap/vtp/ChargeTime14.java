558d249c150d22f419efbb9086eed040b9d32e6c
gurjyan <gurjyan@jlab.org>
2020-11-06 10:55:06 -0500

    - fixed typo in chargeTime14

--- src/main/java/org/jlab/epsci/ersap/vtp/ChargeTime14.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/ChargeTime14.java
@@ -2,4 +2,4 @@ package org.jlab.epsci.ersap.vtp;
 
 import java.math.BigInteger;
 
-public record ChargeTime14 (BigInteger time, int chage) {}
+public record ChargeTime14 (BigInteger time, int charge) {}
