1286269bd939e21a55327e4f7ce5fc4d55898d98
gurjyan <gurjyan@jlab.org>
2020-11-06 10:14:19 -0500

    - AdcHit and ChargeTime records

---
+++ src/main/java/org/jlab/epsci/ersap/vtp/AdcHit14.java
@@ -0,0 +1,3 @@
+package org.jlab.epsci.ersap.vtp;
+
+public record AdcHit14(int crate, int slot, int channel, int q, BigInteger time) {}
---
+++ src/main/java/org/jlab/epsci/ersap/vtp/ChargeTime14.java
@@ -0,0 +1,3 @@
+package org.jlab.epsci.ersap.vtp;
+
+public class ChargeTime14 (BigInteger time, int chage) {}
