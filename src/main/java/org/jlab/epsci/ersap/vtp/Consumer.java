07ce9c71bac8d19aa26d086b21b2efb6c26f9a2e
gurjyan <gurjyan@jlab.org>
2020-11-12 10:57:28 -0500

    - debugging

--- src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
@@ -82,7 +82,7 @@ public class Consumer extends Thread {
 
     public void run() {
         HitFinder hitFinder = new HitFinder();
-        ExecutorService pool = Executors.newFixedThreadPool(4);
+        ExecutorService pool = Executors.newFixedThreadPool(8);
         try {
 
             while (true) {
