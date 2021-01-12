754737a1da85589496b5bc23db316848a9bb9b2d
gurjyan <gurjyan@jlab.org>
2021-01-12 17:08:31 -0500

    - implementing Carl's suggestions with debugging

--- src/main/java/org/jlab/epsci/ersap/vtp/Receiver.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/Receiver.java
@@ -152,7 +152,6 @@ public class Receiver extends Thread {
             dataInputStream.readFully(header);
 //            headerBuffer.flip();
 
-
             int source_id = headerBuffer.getInt();
             int total_length = headerBuffer.getInt();
             int payload_length = headerBuffer.getInt();
@@ -168,9 +167,9 @@ public class Receiver extends Thread {
             BigInteger rcn = EUtil.toUnsignedBigInteger(record_number);
 //                BigInteger tsc = EUtil.toUnsignedBigInteger(ts_sec);
 //                BigInteger tsn = EUtil.toUnsignedBigInteger(ts_nsec);
-//            printFrame(streamId, source_id, total_length, payload_length,
-//                    compressed_length, magic, format_version, flags,
-//                    record_number, ts_sec, ts_nsec);
+            printFrame(streamId, source_id, total_length, payload_length,
+                    compressed_length, magic, format_version, flags,
+                    record_number, ts_sec, ts_nsec);
 
             byte[] dataBuffer = new byte[payload_length];
             dataInputStream.readFully(dataBuffer);
