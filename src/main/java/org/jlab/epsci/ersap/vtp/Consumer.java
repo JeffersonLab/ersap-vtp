f95005fdfbd9f2f79405b60115191ea027c3872a
gurjyan <gurjyan@jlab.org>
2021-01-12 10:34:45 -0500

    - increased data processing threads up to 24

--- src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
@@ -81,7 +81,7 @@ public class Consumer extends Thread {
 
     public void run() {
 //        HitFinder hitFinder = new HitFinder();
-        ExecutorService pool = Executors.newFixedThreadPool(20);
+        ExecutorService pool = Executors.newFixedThreadPool(24);
         try {
 
             while (true) {
