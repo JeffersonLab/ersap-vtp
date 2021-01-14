d8deb0663df208d21dfb4c35f6c661d3b06839ef
gurjyan <gurjyan@jlab.org>
2021-01-14 16:33:53 -0500

    - add redesigned hit finder routine

--- src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/Consumer.java
@@ -81,7 +81,7 @@ public class Consumer extends Thread {
 
     public void run() {
 //        HitFinder hitFinder = new HitFinder();
-        ExecutorService pool = Executors.newFixedThreadPool(2);
+        ExecutorService pool = Executors.newFixedThreadPool(8);
         try {
 
             while (true) {
