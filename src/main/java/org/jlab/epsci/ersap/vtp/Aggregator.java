9b13852c34466df54574ff853bfc30d1aca5dee0
gurjyan <gurjyan@jlab.org>
2021-01-14 17:19:43 -0500

    - debugging

--- src/main/java/org/jlab/epsci/ersap/vtp/Aggregator.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/Aggregator.java
@@ -113,7 +113,8 @@ public class Aggregator extends Thread {
 
             int l1 = inputItem1.getPayloadDataLength();
             int l2 = inputItem2.getPayloadDataLength();
-            System.out.println("DDDD "+ inputItem1.getPayload().length + " " + inputItem2.getPayload().length);
+            System.out.println("DDDD 1 "+ inputItem1.getPayload()[0]+ " " + inputItem1.getPayload()[l1 -1]);
+            System.out.println("DDDD 2 "+ inputItem2.getPayload()[0]+ " " + inputItem2.getPayload()[l2 -1]);
             m1.put(b1, inputItem1.getPayload());
             m2.put(b2, inputItem2.getPayload());
 
