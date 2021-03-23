2772c6a70a427b8f8711357d7ab55639f183fde5
gurjyan <gurjyan@jlab.org>
2021-03-23 14:56:06 -0400

    - introducing an object pool based on disruptor technology

--- src/main/java/org/jlab/epsci/ersap/vtp/EUtil.java
+++ src/main/java/org/jlab/epsci/ersap/vtp/EUtil.java
@@ -332,13 +332,13 @@ public class EUtil {
                                 int channel = (val >> 13) & 0x000F;
                                 long v = ((val >> 17) & 0x3FFF) * 4;
                                 long ht = frame_time_ns + v;
-//                                System.out.println("AdcHit{" +
-//                                        "crate=" + crate +
-//                                        ", slot=" + slot +
-//                                        ", channel=" + channel +
-//                                        ", q=" + q +
-//                                        ", time=" + ht +
-//                                        '}');
+                                System.out.println("AdcHit{" +
+                                        "crate=" + crate +
+                                        ", slot=" + slot +
+                                        ", channel=" + channel +
+                                        ", q=" + q +
+                                        ", time=" + ht +
+                                        '}');
                             }
                         }
                     }
