e85a46cf260393e051c5bf4c6f58a5dfdb303f2a
gurjyan <gurjyan@jlab.org>
2021-06-02 15:30:19 -0400

    -debugging...

--- src/main/java/org/jlab/epsci/ersap/sampa/SDecoder.java
+++ src/main/java/org/jlab/epsci/ersap/sampa/SDecoder.java
@@ -75,13 +75,14 @@ public class SDecoder {
             for (int ii = ii_max; ii >= ii_min; ii--) {
                 // elink (4 bits per frame)
                 bitValue = (gFrameWord & (0x00000001 << ii)) >>> ii;
-                System.out.println( "DDD-> "+ ii + " " + Integer.toHexString(gFrameWord) + " " + Integer.toHexString(bitValue));
+
                 if (bitValue == 1) {
                     shiftReg[eLink] = shiftReg[eLink] | 0x0004000000000000L; // set bit 50 in shiftReg
                 }
                 shiftReg[eLink] = shiftReg[eLink] >>> 1;
 
                 if (eLink == 2) {
+                    System.out.println( "DDD-> "+ ii + " " + Integer.toHexString(gFrameWord) + " " + Integer.toHexString(bitValue));
                     System.out.println("DDD: elink = " + eLink +
                             " shiftReg = " +Long.toHexString(shiftReg[eLink]));
                 }
