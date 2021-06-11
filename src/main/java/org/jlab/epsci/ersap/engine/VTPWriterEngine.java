653e3dc2ddf39bd942a27fe301588f6f661ea177
gurjyan <gurjyan@jlab.org>
2021-06-11 16:49:58 -0400

    -yaml service description files

--- src/main/java/org/jlab/epsci/ersap/engine/VTPWriterEngine.java
+++ src/main/java/org/jlab/epsci/ersap/engine/VTPWriterEngine.java
@@ -3,6 +3,7 @@ package org.jlab.epsci.ersap.engine;
 import org.jlab.clara.engine.EngineDataType;
 import org.jlab.clara.std.services.AbstractEventWriterService;
 import org.jlab.clara.std.services.EventWriterException;
+import org.jlab.epsci.ersap.engine.util.StreamingDataTypes;
 import org.json.JSONObject;
 
 import java.nio.file.Path;
@@ -26,6 +27,6 @@ public class VTPWriterEngine extends AbstractEventWriterService<Object> {
 
     @Override
     protected EngineDataType getDataType() {
-        return null;
+        return StreamingDataTypes.VTP_G;
     }
 }
