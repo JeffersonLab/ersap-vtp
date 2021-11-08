74adf24f3a53d47beb769ed2e4d82534f2bf3daf
gurjyan <gurjyan@jlab.org>
2021-11-08 13:32:25 -0500

    - test of small pools

--- src/main/java/org/jlab/epsci/stream/vtp/VConsumer.java
+++ src/main/java/org/jlab/epsci/stream/vtp/VConsumer.java
@@ -39,8 +39,8 @@ public class VConsumer extends Thread {
         nextSequence = sequence.get() + 1L;
         availableSequence = -1L;
 
-        tPool = Executors.newFixedThreadPool(24);
-        pool = createPdPool(24);
+        tPool = Executors.newFixedThreadPool(2);
+        pool = createPdPool(2);
 
     }
 
