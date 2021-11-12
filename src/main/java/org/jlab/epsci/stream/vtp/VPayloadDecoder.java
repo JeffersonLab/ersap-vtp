8e9dd96b4111a65a0eef3da910de3c18d6f4d6bd
gurjyan <gurjyan@jlab.org>
2021-11-11 21:48:13 -0500

    - coincidence algorithm

--- src/main/java/org/jlab/epsci/stream/vtp/VPayloadDecoder.java
+++ src/main/java/org/jlab/epsci/stream/vtp/VPayloadDecoder.java
@@ -44,7 +44,7 @@ public class VPayloadDecoder {
         buf.clear();
         corePayloadDecoder(frame_time_ns, pData, 0);
 //        dump(evt.getEvList()); // dump entire frame
-        coincidence(0,2); // print coincidences within 50 ns window
+        coincidence(1,2); // print coincidences within 50 ns window
 
     }
 
