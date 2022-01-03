47e93473cf5f0ce216e9be1356d6306371bf4e75
gurjyan <gurjyan@jlab.org>
2022-01-03 16:07:51 -0500

    - Draw analyzed data from the HIPO file. - Analysis: 1) It must be one and only one hit per channel withing 16ns (sliding window: event), 2) Central channel energy deposition must be bigger than left or right channels

--- src/main/java/org/jlab/epsci/stream/vtp/HipoDrawBank.java
+++ src/main/java/org/jlab/epsci/stream/vtp/HipoDrawBank.java
@@ -31,15 +31,15 @@ public class HipoDrawBank {
         Bank raw = r.getBank("raw::data");
         Event event = new Event();
 
-        H1F hl = new H1F("h1", 100, 0.0, 8000.0);
-        H1F hc = new H1F("h2", 100, 0.0, 8000.0);
-        H1F hr = new H1F("h2", 100, 0.0, 8000.0);
-        H1F ha = new H1F("h1", 100, 0.0, 8000.0);
-        H1F hb = new H1F("h2", 100, 0.0, 8000.0);
-        H1F hcc = new H1F("h2", 100, 0.0, 8000.0);
-        H1F hd = new H1F("h1", 100, 0.0, 8000.0);
-        H1F he = new H1F("h2", 100, 0.0, 8000.0);
-        H1F hf = new H1F("h2", 100, 0.0, 8000.0);
+        H1F hl = new H1F("h1", 100, 2000.0, 8000.0);
+        H1F hc = new H1F("h2", 100, 2000.0, 8000.0);
+        H1F hr = new H1F("h2", 100, 2000.0, 8000.0);
+        H1F ha = new H1F("h1", 100, 2000.0, 8000.0);
+        H1F hb = new H1F("h2", 100, 2000.0, 8000.0);
+        H1F hcc = new H1F("h2", 100, 2000.0, 8000.0);
+        H1F hd = new H1F("h1", 100, 2000.0, 8000.0);
+        H1F he = new H1F("h2", 100, 2000.0, 8000.0);
+        H1F hf = new H1F("h2", 100, 2000.0, 8000.0);
 //        H2F h2 = new H2F("h2", 112, 0.0, 112.0, 60, 0.0, 500.0);
 
         List<Integer> a = new ArrayList<>();
