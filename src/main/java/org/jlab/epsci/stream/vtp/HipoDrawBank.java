b36fea208605c5b37d8828aded92319621d3b1d8
gurjyan <gurjyan@jlab.org>
2022-01-03 15:31:10 -0500

    - Draw analyzed data from the HIPO file. - Analysis: 1) It must be one and only one hit per channel withing 16ns (sliding window: event), 2) Central channel energy deposition must be bigger than left or right channels

--- src/main/java/org/jlab/epsci/stream/vtp/HipoDrawBank.java
+++ src/main/java/org/jlab/epsci/stream/vtp/HipoDrawBank.java
@@ -120,16 +120,16 @@ public class HipoDrawBank {
         }
 
         TGCanvas c = new TGCanvas(800, 500);
-        c.view().divide(3, 3);
-        c.view().region(0).draw(ha);
-        c.view().region(1).draw(hb);
-        c.view().region(2).draw(hcc);
+        c.view().divide(1, 3);
+//        c.view().region(0).draw(ha);
+//        c.view().region(1).draw(hb);
+//        c.view().region(2).draw(hcc);
         c.view().region(3).draw(hl);
         c.view().region(4).draw(hc);
         c.view().region(5).draw(hr);
-        c.view().region(6).draw(hd);
-        c.view().region(7).draw(he);
-        c.view().region(8).draw(hf);
+//        c.view().region(6).draw(hd);
+//        c.view().region(7).draw(he);
+//        c.view().region(8).draw(hf);
         c.repaint();
     }
 }
