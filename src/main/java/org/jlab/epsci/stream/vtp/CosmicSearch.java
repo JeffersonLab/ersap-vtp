a9674c17a8d3ea00667901356347c1a83c5cde78
gurjyan <gurjyan@jlab.org>
2022-01-05 11:13:38 -0500

    - Cosmic search trigger engine algorithm

--- src/main/java/org/jlab/epsci/stream/vtp/CosmicSearch.java
+++ src/main/java/org/jlab/epsci/stream/vtp/CosmicSearch.java
@@ -40,15 +40,15 @@ public class CosmicSearch {
         Bank raw = r.getBank("raw::data");
         Event event = new Event();
 
-        H1F hl = new H1F("left", 100, 0.0, 1000.0);
-        H1F hc = new H1F("center", 100, 0.0, 1000.0);
-        H1F hr = new H1F("right", 100, 0.0, 1000.0);
-        H1F ha = new H1F("a", 100, 0.0, 1000.0);
-        H1F hb = new H1F("b", 100, 0.0, 1000.0);
-        H1F hcc = new H1F("cc", 100, 0.0, 1000.0);
-        H1F hd = new H1F("d", 100, 0.0, 1000.0);
-        H1F he = new H1F("e", 100, 0.0, 1000.0);
-        H1F hf = new H1F("f", 100, 0.0, 1000.0);
+        H1F hl = new H1F("left", 100, 0.0, 8000.0);
+        H1F hc = new H1F("center", 100, 0.0, 8000.0);
+        H1F hr = new H1F("right", 100, 0.0, 8000.0);
+        H1F ha = new H1F("a", 100, 0.0, 8000.0);
+        H1F hb = new H1F("b", 100, 0.0, 8000.0);
+        H1F hcc = new H1F("cc", 100, 0.0, 8000.0);
+        H1F hd = new H1F("d", 100, 0.0, 8000.0);
+        H1F he = new H1F("e", 100, 0.0, 8000.0);
+        H1F hf = new H1F("f", 100, 0.0, 8000.0);
 //        H2F h2 = new H2F("h2", 112, 0.0, 112.0, 60, 0.0, 500.0);
 
         List<Integer> a = new ArrayList<>();
