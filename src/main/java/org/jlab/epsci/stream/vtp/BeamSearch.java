9bc236e2365a047f522d332094ae570fc0de986f
gurjyan <gurjyan@jlab.org>
2022-01-07 11:18:26 -0500

    - histo for sum

--- src/main/java/org/jlab/epsci/stream/vtp/BeamSearch.java
+++ src/main/java/org/jlab/epsci/stream/vtp/BeamSearch.java
@@ -6,6 +6,9 @@ import j4np.hipo5.io.HipoReader;
 import twig.data.H1F;
 import twig.data.H2F;
 import twig.graphics.TGCanvas;
+import twig.math.DataFitter;
+import twig.math.F1D;
+import twig.widgets.PaveText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -162,8 +165,24 @@ public class BeamSearch {
         TGCanvas c;
         if (s) {
             c = new TGCanvas(800, 1000);
-            c.view().divide(1, 1);
+            c.view().divide(2, 1);
             c.view().region(0).draw(hsum);
+            F1D func = new F1D("func","[a]+[b]*x+[c]*x*x+[d]*gaus(x,[e],[f])",3500,6600);
+            func.setParameters(new double[]{1.0,1.0,1.0,10000,6000,500});
+
+            func.setParLimits(3,0,40000);
+            func.setParLimits(4,5500,6500);
+            func.setParLimits(5,0.0,1000);
+            func.attr().setLineWidth(2);
+            DataFitter.fit(func,hsum,"N");
+
+//            PaveText paveStats = new PaveText(func.getStats("M"),0.05,0.95, false,18);
+//            paveStats.setNDF(true).setMultiLine(true);
+
+//            c.view().region(1).draw(hsum).draw(func,"same").draw(paveStats);
+            c.view().region(1).draw(hsum).draw(func,"same");
+            c.repaint();
+
         } else {
             c = new TGCanvas(1600, 1000);
             c.view().divide(3, 3);
