ce453569e3e12e69cb96a218e21a48e21cd9dfc5
gurjyan <gurjyan@jlab.org>
2022-01-07 12:54:38 -0500

    - histo for sum

--- src/main/java/org/jlab/epsci/stream/vtp/CosmicSearch.java
+++ src/main/java/org/jlab/epsci/stream/vtp/CosmicSearch.java
@@ -231,7 +231,7 @@ public class CosmicSearch {
 //            F1D func = new F1D("func","[a]*gaus(x,[b],[c])+[d]+[e]*x+[f]*x*x",100,1000);
 //            func.setParameters(new double[]{5000,600,500,1.0,1.0,1.0});
             F1D func = new F1D("func","[a]*gaus(x,[b],[c])",100,800);
-            func.setParameters(new double[]{5000,600,500});
+            func.setParameters(new double[]{5000,300,200});
             func.setParLimits(0,0,5000);
             func.setParLimits(1,0,800);
             func.setParLimits(2,0,1000);
