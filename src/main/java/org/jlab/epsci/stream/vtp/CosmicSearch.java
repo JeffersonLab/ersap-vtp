cb2ab7151785082589de3500edf52db2d489163d
gurjyan <gurjyan@jlab.org>
2022-01-07 20:54:37 -0500

    - histo for sum

--- src/main/java/org/jlab/epsci/stream/vtp/CosmicSearch.java
+++ src/main/java/org/jlab/epsci/stream/vtp/CosmicSearch.java
@@ -157,7 +157,7 @@ public class CosmicSearch {
 //                            & f.size() == 0
                     ) {
                         if((a.get(0) == d.get(0))
-                                && (left.get(0) == d.get(0))
+//                                && (left.get(0) == d.get(0))
                         )
 
                             //                        if ((a.get(0) >= left.get(0))
