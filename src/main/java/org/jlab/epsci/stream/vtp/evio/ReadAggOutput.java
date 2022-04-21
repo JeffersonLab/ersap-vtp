a6d7bfd26e12c55ca59d9cf8680173bf2ab5eda3
gurjyan <gurjyan@jlab.org>
2022-04-21 13:36:00 -0400

    - update reader from Carl

--- src/main/java/org/jlab/epsci/stream/vtp/evio/ReadAggOutput.java
+++ src/main/java/org/jlab/epsci/stream/vtp/evio/ReadAggOutput.java
@@ -16,6 +16,7 @@ package org.jlab.epsci.stream.vtp.evio;
 import org.jlab.coda.jevio.*;
 import org.jlab.epsci.stream.vtp.VAdcHit;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
@@ -26,38 +27,54 @@ public class ReadAggOutput {
     static int readFile(String finalFilename) throws Exception {
 
         try {
-            EvioReader reader = new EvioReader(finalFilename);
+            EvioReader reader = new EvioReader(new File(finalFilename), false, true, false);
             ByteOrder order = reader.getByteOrder();
-
 
             int evCount = reader.getEventCount();
             System.out.println("Read in file " + finalFilename + ", got " + evCount + " events");
 
-            // Loop through all events
-            for (int i = 3; i <= evCount; i++) {
+            // Loop through all events (skip first 2 which are prestart and go)
+            for (int i=1; i <= evCount; i++) {
 
                 System.out.println("Event " + i + ":");
-                EvioEvent ev = reader.getEvent(i);
-                ev.setByteOrder(ByteOrder.LITTLE_ENDIAN);
-                System.out.println(String.format("%x", ev.getHeader().getTag()));
+                EvioEvent ev = reader.parseEvent(i);
+                int evTag = ev.getHeader().getTag();
+                if (evTag == 0xffd1) {
+                    System.out.println("Skip over PRESTART event");
+                    continue;
+                }
+                else if (evTag == 0xffd2) {
+                    System.out.println("Skip over GO event");
+                    continue;
+                }
+                else if (evTag == 0xffd4) {
+                    System.out.println("Hit END event, quitting");
+                    break;
+                }
+
+                if (evTag == 0xff60) {
+                    System.out.println("Found built streaming event");
+                }
+
                 // Go one level down ->
                 int childCount = ev.getChildCount();
                 if (childCount < 2) {
                     throw new Exception("Problem: too few child for event (" + childCount + ")");
                 }
+                System.out.println("Event has " + childCount + " child structures");
 
                 // First bank is Time Info Bank (TIB) with frame and timestamp
                 EvioBank b = (EvioBank) ev.getChildAt(0);
                 int[] intData = b.getIntData();
                 int frame = intData[0];
-                long timestamp = ((((long) intData[1]) & 0x00000000ffffffffL) +
-                        (((long) intData[2]) << 32));
+                long timestamp = ((((long)intData[1]) & 0x00000000ffffffffL) +
+                        (((long)intData[2]) << 32));
                 System.out.println("  Frame = " + frame + ", TS = " + timestamp);
 
                 // Loop through all ROC Time Slice Banks (TSB) which come after TIB
-                for (int j = 1; j < childCount; j++) {
+                for (int j=1; j < childCount; j++) {
                     // ROC Time SLice Bank
-                    EvioBank rocTSB = (EvioBank) ev.getChildAt(j);
+                    EvioBank rocTSB = (EvioBank)ev.getChildAt(j);
                     int kids = rocTSB.getChildCount();
                     if (kids < 2) {
                         throw new Exception("Problem: too few child for TSB (" + childCount + ")");
@@ -67,31 +84,31 @@ public class ReadAggOutput {
                     // followed by data banks
 
                     // Skip over SIB by starting at 1
-                    for (int k = 1; k < kids; k++) {
+                    for (int k=1; k < kids; k++) {
                         EvioBank dataBank = (EvioBank) rocTSB.getChildAt(k);
-                        // here is where you get data
-                        DataType type = dataBank.getHeader().getDataType();
-                        System.out.println("    Data type in data bank " + (k - 1) +
-                                " is " + type.toString());
-                        switch (type) {
-                            case INT32:
-                                // do some stuff
-                                int[] iData = dataBank.getIntData();
-                                List<VAdcHit> hits = fADCPayloadDecoder(timestamp, iData);
-                                for (VAdcHit h : hits) {
-                                    System.out.println(h);
-                                }
-                                break;
-                            default:
-                        }
+                        // Ignore the data type (currently the improper value of 0xf).
+                        // Just get the data as bytes
+                        byte[] byteData = dataBank.getRawBytes();
+//                        List<VAdcHit> hits = fADCPayloadDecoder(timestamp, iData);
+//                        for (VAdcHit h : hits) {
+//                            System.out.println(h);
+//                        }
+
                     }
                 }
             }
-        } catch (IOException | EvioException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
+        catch (EvioException e) {
+            e.printStackTrace();
+        }
+
         return 0;
     }
+
+
 
     public static List<VAdcHit> fADCPayloadDecoder(Long frame_time_ns,
                                                    int[] pData) {
