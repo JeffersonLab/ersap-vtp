56b8f7354270471f442cae45a4bb2c0050ae94fd
gurjyan <gurjyan@jlab.org>
2022-04-24 12:04:44 -0400

    creating engines for the VTP EVIO fADC decoding

--- src/main/java/org/jlab/epsci/stream/engine/evio/EvioFadcDecoderEngine.java
+++ src/main/java/org/jlab/epsci/stream/engine/evio/EvioFadcDecoderEngine.java
@@ -1,5 +1,15 @@
 package org.jlab.epsci.stream.engine.evio;
-
+/**
+ * Copyright (c) 2021, Jefferson Science Associates, all rights reserved.
+ * See LICENSE.txt file.
+ * Thomas Jefferson National Accelerator Facility
+ * Experimental Physics Software and Computing Infrastructure Group
+ * 12000, Jefferson Ave, Newport News, VA 23606
+ * Phone : (757)-269-7100
+ *
+ * @author gurjyan on 4/23/22
+ * @project ersap-vtp
+ */
 import org.jlab.coda.jevio.EvioBank;
 import org.jlab.coda.jevio.EvioEvent;
 import org.jlab.epsci.ersap.base.ErsapUtil;
@@ -20,15 +30,11 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * Copyright (c) 2021, Jefferson Science Associates, all rights reserved.
- * See LICENSE.txt file.
- * Thomas Jefferson National Accelerator Facility
- * Experimental Physics Software and Computing Infrastructure Group
- * 12000, Jefferson Ave, Newport News, VA 23606
- * Phone : (757)-269-7100
- *
- * @author gurjyan on 4/23/22
- * @project ersap-vtp
+ * ERSAP engine that accepts EvioEvent: a build streaming event,
+ * contains prestart and go events, followed by the TIB (trigger info bank).
+ * TIB has rock time-slice bank (TSB), followed by stream info bank (SIB).
+ * in SIB we get hits (channel, charge and time) reported by each payload port (slot).
+ * Note that this class considers data from a single ROC.
  */
 public class EvioFadcDecoderEngine implements Engine {
 
@@ -36,6 +42,10 @@ public class EvioFadcDecoderEngine implements Engine {
     private int slidingWindowSize;
     private static String S_STEP = "sliding_step";
     private int stepSize;
+    // expected beam_center = "crate_slot_channel"
+    private static String BEAM_CENTER = "beam_center";
+    private String beamCenter;
+    private static String UNDEFINED = "undefined";
 
     private static int[] slotMap = {0, 10, 13, 9, 14, 8, 15, 7, 16, 6, 17, 5, 18, 4, 19, 3, 20};
 
@@ -46,6 +56,11 @@ public class EvioFadcDecoderEngine implements Engine {
             JSONObject data = new JSONObject(source);
             slidingWindowSize = data.has(S_WINDOW) ? data.getInt(S_WINDOW) : 16;
             stepSize = data.has(S_STEP) ? data.getInt(S_STEP) : 1;
+            if(data.has(BEAM_CENTER)){
+                beamCenter = data.getString(BEAM_CENTER);
+            } else {
+                beamCenter = UNDEFINED;
+            }
         }
 
 //        // Histogram preparation
@@ -146,9 +161,16 @@ public class EvioFadcDecoderEngine implements Engine {
         return input;
     }
 
+    /**
+     * Finds the hits (channel, charge and time) reported by the fADC in a specific slot
+     * @param data
+     * @param frame_time_ns
+     * @param slot
+     * @param ba
+     */
     private void fADCPayloadDecoder(Map<Long, List<VAdcHit>> data,
                                     Long frame_time_ns,
-                                    int payloadId,
+                                    int slot,
                                     byte[] ba) {
         IntBuffer intBuf =
                 ByteBuffer.wrap(ba)
@@ -162,15 +184,21 @@ public class EvioFadcDecoderEngine implements Engine {
             long v = ((i >> 17) & 0x3FFF) * 4;
             long ht = frame_time_ns + v;
             if (data.containsKey(ht)) {
-                data.get(ht).add(new VAdcHit(1, payloadId, channel, q, ht));
+                data.get(ht).add(new VAdcHit(1, slot, channel, q, ht));
             } else {
                 List<VAdcHit> hits = new ArrayList<>();
-                hits.add(new VAdcHit(1, payloadId, channel, q, ht));
+                hits.add(new VAdcHit(1, slot, channel, q, ht));
                 data.put(ht, hits);
             }
         }
     }
 
+    /**
+     * Sliding window algorithm that requires single hit per channel in the sliding window,
+     * and that the channel that corresponds to the beam center would have a largest charge.
+     * @param hits
+     * @return
+     */
     private Map<String, List<Integer>> eventIdentification(Map<Long, List<VAdcHit>> hits) {
         Map<String, List<Integer>> evIdentified = new HashMap<>();
         long sTime, eTime;
@@ -179,23 +207,28 @@ public class EvioFadcDecoderEngine implements Engine {
         long startFrameTime = Collections.min(timeStamps);
         long endFrameTime = Collections.max(timeStamps);
         do {
+            // sliding time window leading edge
             sTime = startFrameTime + ((long) step * stepSize);
+            // sliding time window trailing edge
             eTime = sTime + slidingWindowSize;
             step++;
             final long s = sTime;
             final long e = eTime;
 
+            // carve the data for that window from the VTP frame
             Map<Long, List<VAdcHit>> subMap = hits.entrySet().stream()
                     .filter(x -> (x.getKey() >= s) && (x.getKey() <= e))
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
             // event identification
+            // store hits within the sliding window in a new container: name and the list of hits
             Map<String, List<Integer>> slidingWindowHits = new HashMap<>();
             for (List<VAdcHit> l : subMap.values()) {
                 for (VAdcHit h : l) {
+                    // the name of the channel: crate_slot_channel
                     String n = h.getCrate()
                             + "_" + h.getSlot()
-                            + " " + h.getCrate();
+                            + "_" + h.getChannel();
 
                     if (slidingWindowHits.containsKey(n)) {
                         slidingWindowHits.get(n).add(h.getCharge());
@@ -208,6 +241,7 @@ public class EvioFadcDecoderEngine implements Engine {
             }
             boolean foundEvent = true;
             Map<String, Integer> evtCandidate = new HashMap<>();
+            // if there are more than 1 hit per channel fail the identification
             for (var entry : slidingWindowHits.entrySet()) {
                 if (entry.getValue().size() > 1) {
                     foundEvent = false;
@@ -215,6 +249,16 @@ public class EvioFadcDecoderEngine implements Engine {
                     break;
                 } else {
                     evtCandidate.put(entry.getKey(), entry.getValue().get(0));
+                }
+            }
+            // require beamCenter to have the max deposited charge
+            if(!beamCenter.equals(UNDEFINED)) {
+                int maxValueInMap = (Collections.max(evtCandidate.values()));
+                for (var entry : evtCandidate.entrySet()) {
+                    if ((entry.getValue() == maxValueInMap) && !(entry.getKey().equals(beamCenter))) {
+                        foundEvent = false;
+                        break;
+                    }
                 }
             }
             if (foundEvent) {
@@ -260,7 +304,7 @@ public class EvioFadcDecoderEngine implements Engine {
 
     @Override
     public String getDescription() {
-        return "fADC data decoder. EVIO data format";
+        return "fADC data decoder and event identification. EVIO data format";
     }
 
     @Override
