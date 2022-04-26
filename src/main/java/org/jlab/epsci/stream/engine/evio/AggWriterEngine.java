22daaa7499c1a8cd975ac302d5bb95404076313d
gurjyan <gurjyan@jlab.org>
2022-04-26 14:52:43 -0400

    debugging EvioFadcDecoderEngine

--- src/main/java/org/jlab/epsci/stream/engine/evio/AggWriterEngine.java
+++ src/main/java/org/jlab/epsci/stream/engine/evio/AggWriterEngine.java
@@ -4,13 +4,16 @@ import org.jlab.epsci.ersap.engine.EngineDataType;
 import org.jlab.epsci.ersap.std.services.AbstractEventWriterService;
 import org.jlab.epsci.ersap.std.services.EventWriterException;
 import org.jlab.epsci.stream.engine.util.JavaObjectType;
+import org.jlab.epsci.stream.vtp.evio.LiveHistogram;
 import org.json.JSONObject;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 /**
  * Copyright (c) 2021, Jefferson Science Associates, all rights reserved.
@@ -24,9 +27,61 @@ import java.util.Map;
  * @project ersap-vtp
  */
 public class AggWriterEngine extends AbstractEventWriterService<FileWriter> {
+    private static String FRAME_TITLE = "frame_title";
+    private String frameTitle;
+    private static String FRAME_WIDTH = "frame_width";
+    private int frameWidth;
+    private static String FRAME_HEIGHT = "frame_height";
+    private int frameHeight;
+    private static String HIST_TITLES = "hist_titles";
+    private ArrayList<String> histTitles;
+    private static String HIST_BINS = "hist_bins";
+    private int histBins;
+    private static String HIST_MIN = "hist_min";
+    private double histMin;
+    private static String HIST_MAX = "hist_max";
+    private double histMax;
+    private static String GRID_SIZE = "grid_size";
+    private int gridSize;
+
+    private LiveHistogram liveHist;
+
     @Override
     protected FileWriter createWriter(Path file, JSONObject opts)
             throws EventWriterException {
+        if (opts.has(FRAME_TITLE) ) {
+            frameTitle = opts.getString(FRAME_TITLE);
+        }
+        if (opts.has(FRAME_WIDTH) ) {
+            frameWidth = opts.getInt(FRAME_WIDTH);
+        }
+        if (opts.has(FRAME_HEIGHT) ) {
+            frameHeight = opts.getInt(FRAME_HEIGHT);
+        }
+        if (opts.has(HIST_TITLES) ) {
+            histTitles = new ArrayList<>();
+            String ht = opts.getString(HIST_TITLES);
+            StringTokenizer st = new StringTokenizer(ht,",");
+            while (st.hasMoreTokens()){
+                histTitles.add(st.nextToken());
+            }
+        }
+        if (opts.has(HIST_BINS) ) {
+            histBins = opts.getInt(HIST_BINS);
+        }
+        if (opts.has(HIST_MIN) ) {
+            histMin = opts.getDouble(HIST_MIN);
+        }
+        if (opts.has(HIST_MAX) ) {
+            histMax = opts.getDouble(HIST_MAX);
+        }
+        if (opts.has(GRID_SIZE) ) {
+            gridSize = opts.getInt(GRID_SIZE);
+        }
+
+        liveHist = new LiveHistogram(frameTitle, histTitles, gridSize,
+                frameWidth, frameHeight, histBins, histMin, histMax);
+
         try {
             return new FileWriter(file.toString());
         } catch (IOException e) {
@@ -47,7 +102,10 @@ public class AggWriterEngine extends AbstractEventWriterService<FileWriter> {
     protected void writeEvent(Object event) throws EventWriterException {
         Map<String, List<Integer>> evIdentified = (Map<String, List<Integer>>)event;
         for(String s:evIdentified.keySet()){
-            System.out.println("DDD "+s+ " "+evIdentified.get(s));
+            System.out.println("DDD "+s+ " "+evIdentified.get(s).size());
+            for(Integer charge: evIdentified.get(s)) {
+                liveHist.update(s, charge);
+            }
         }
     }
 
--- src/main/java/org/jlab/epsci/stream/vtp/evio/LiveHistogram.java
+++ src/main/java/org/jlab/epsci/stream/vtp/evio/LiveHistogram.java
@@ -4,6 +4,10 @@ import twig.data.H1F;
 import twig.graphics.TGDataCanvas;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -18,19 +22,79 @@ import java.util.Random;
  * @project ersap-vtp
  */
 public class LiveHistogram {
+
+    private Map<String, H1F> histograms = new HashMap<>();
+
+    public LiveHistogram(String frameTitle, ArrayList<String> histTitles,
+                         int gridSize, int frameWidth, int frameHeight,
+                         int histBins, double histMin, double histMax) {
+
+        JFrame frame = new JFrame(frameTitle);
+        frame.setSize(frameWidth, frameHeight);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(gridSize,gridSize));
+        frame.getContentPane().add(panel);
+
+        // create canvases with associated histograms,
+        // and add them to the panel
+        for(String s: histTitles){
+            TGDataCanvas c = new TGDataCanvas();
+            c.initTimer(600);
+            H1F hist = new H1F(s, histBins, histMin, histMax);
+            histograms.put(s, hist);
+            c.region().draw(hist);
+            panel.add(c);
+        }
+    }
+
+    public void update (String name, int value) {
+        if(histograms.containsKey(name)){
+            histograms.get(name).fill(value);
+        }
+    }
+
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Canvas");
-        TGDataCanvas c = new TGDataCanvas();
+        JFrame frame = new JFrame("ERSAP");
+        frame.setSize(1200, 600);
+        /////
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2,2));
+        frame.getContentPane().add(panel);
+        /////
 
-        frame.add(c);
-        frame.setSize(600, 600);
+        TGDataCanvas c1 = new TGDataCanvas();
+        TGDataCanvas c2 = new TGDataCanvas();
+        TGDataCanvas c3 = new TGDataCanvas();
+        TGDataCanvas c4 = new TGDataCanvas();
+
+        ///
+        panel.add(c1);
+        panel.add(c2);
+        panel.add(c3);
+        panel.add(c4);
+        ///
+
+//        frame.add(c);
+//        frame.setSize(600, 600);
         frame.setVisible(true);
 
-        c.initTimer(600);
-        H1F h = new H1F("h", 100, -3.0, 3.0);
+        c1.initTimer(600);
+        H1F h1 = new H1F("h1", 100, -3.0, 3.0);
+        c1.region().draw(h1);
 
-        c.region().draw(h);
+        c2.initTimer(600);
+        H1F h2 = new H1F("h2", 100, -3.0, 3.0);
+        c2.region().draw(h2);
+
+        c3.initTimer(600);
+        H1F h3 = new H1F("h3", 100, -3.0, 3.0);
+        c3.region().draw(h3);
+
+        c4.initTimer(600);
+        H1F h4 = new H1F("h4", 100, -3.0, 3.0);
+        c4.region().draw(h4);
+
         Random r = new Random();
         while (true) {
             try {
@@ -38,7 +102,12 @@ public class LiveHistogram {
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
-            for (int k = 0; k < 10; k++) h.fill(r.nextGaussian());
+            for (int k = 0; k < 10; k++) {
+                h1.fill(r.nextGaussian());
+                h2.fill(r.nextGaussian());
+                h3.fill(r.nextGaussian());
+                h4.fill(r.nextGaussian());
+            }
         }
     }
 }
